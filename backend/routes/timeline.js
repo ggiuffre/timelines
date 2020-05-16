db9fff10616a859baec2b300a93ddb6441d521f8
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-05-16 17:45:27 +0200

    fix bugs with containerization

--- backend/routes/timeline.js
+++ backend/routes/timeline.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://mongo:27017/test';
+const url = 'mongodb://mongodb:27017/test';
 const dbOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 const client = new MongoClient(url, dbOptions);
 
