bfe32f8025ee70d1dcba3f1e427b0a490bbe4941
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-05-20 10:15:36 +0200

    refactor CSS rules into different files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Timeline from './Timeline.js';
-import './Timeline.css';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
--- frontend/src/Technology.js
+++ frontend/src/Technology.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Timeline.css';
+import './Technology.css';
 
 class Technology extends React.Component {
   constructor(props) {
