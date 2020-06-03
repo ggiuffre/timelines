241b761910e34cfe924c5c755e9a803be1139d5e
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2020-06-03 22:39:14 +0200

    clean and improve back-end logic

--- backend/app.js
+++ backend/app.js
@@ -4,42 +4,40 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter    = require('./routes/index');
+// declare the main API route:
 const timelineRouter = require('./routes/timeline');
 
+// declare the app object:
 const app = express();
 
+// allow CORS: (TODO fine-tune for production)
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+// mount middleware and other components:
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
 app.use('/timeline', timelineRouter);
 
-// catch 404 and forward to error handler
+// catch 404s and forward to an error handler:
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// handle errors:
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // set locals, only providing error in development:
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error page:
   res.status(err.status || 500);
   res.render('error');
 });
